6a94e2cb4e7ab1ceea139c91d6dc744d110d8166
Jtclee85 <jbeduwork@gmail.com>
2025-07-02 23:16:05 +0900

    Create Banner.js

---
+++ components/Banner.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Banner.module.css';
+import Image from 'next/image';
+import bannerImage from '../public/banner-background.png'; // 
+
+const Banner = () => {
+return (
+
+
+뭐냐면
+조사한 원본 자료를 쉽고 재미있게 설명해주는 AI 친구
+
+export default Banner;
